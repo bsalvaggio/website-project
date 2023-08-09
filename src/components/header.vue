eda4aa569a1836f834fd3e8e6fde2e464123c596
bsalvaggio <119374432+bsalvaggio@users.noreply.github.com>
2023-08-09 14:35:57 -0400

    Update header.vue

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,14 +1,13 @@
 <template>
     <header class="header">
       <img src="@/assets/face.jpg" alt="Logo" class="logo" />
-      <nav>
+      <nav class="nav-container">
         <router-link to="/" class="nav-link">Home</router-link>
         <router-link to="/resume" class="nav-link">Resume</router-link>
         <router-link to="/projects" class="nav-link">Projects</router-link>
         <router-link to="/articles" class="nav-link">Articles</router-link>
         <router-link to="/about" class="nav-link">About</router-link>
         <router-link to="/contact" class="nav-link">Contact</router-link>
-
       </nav>
     </header>
   </template>
@@ -20,34 +19,56 @@
   </script>
   
   <style scoped>
-  .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem 2rem;
-    background-color: #070707;
-  }
-  
-  .logo {
-    width: 50px;
-  }
-  
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  background-color: #070707;
+  flex-wrap: wrap;
+}
+
+.logo {
+  width: 50px;
+  margin-right: 1rem;
+}
+
+.nav-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.nav-link {
+  color: white;
+  margin-left: 1rem;
+  text-decoration: none;
+  font-size: 1.2rem; /* Set an explicit font size for larger screens */
+}
+
+.nav-link.router-link-exact-active {
+  font-weight: bold;
+}
+
+/* Media query for medium screens (tablets) */
+@media (max-width: 992px) {
   .nav-link {
-    color: white;
-    margin-left: 1rem;
-    text-decoration: none;
+    font-size: 1rem; /* Adjust font size */
   }
-  
-  .nav-link.router-link-exact-active {
-    font-weight: bold;  /* you can style the active link differently, e.g., with bold font */
-  }
-  </style>
-  <style scoped>
+
   .logo {
-      width: auto;      /* Adjust as necessary */
-      max-width: 18%;  /* Ensures the image doesn't overflow its container */
-      height: auto;     /* Adjust if you want a fixed height */
-      /* Add any additional styles for alignment, margin, etc. if necessary */
+    width: 45px; 
   }
-  </style>
-  
+}
+
+/* Media query for mobile view */
+@media (max-width: 768px) {
+  .nav-link {
+    font-size: 0.8rem; /* Further reduce font size for smaller screens */
+    margin-left: 0.5rem;
+  }
+
+  .logo {
+    width: 40px;
+  }
+}
+</style>

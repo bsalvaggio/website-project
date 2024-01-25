56b7cc8a265118078cd615185e8e8bfd69f98ac4
bsalvaggio <119374432+bsalvaggio@users.noreply.github.com>
2024-01-25 10:09:05 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
     <div>  
       <h1>About This AWS Cloud Resume Website Project</h1>
-      <p> </p>
       <img class="top-image" src="@/assets/website_diagram.png" alt="architecture diagram" style="width: 100%; height: auto;" />        
+      <p> </p>
       <a href="https://medium.com/@bill.salvaggio/the-aws-cloud-resume-challenge-project-c5c0c6fe9593" 
            target="_blank" 
            rel="noopener"
